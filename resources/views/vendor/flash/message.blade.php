a1a52aafea8df650ce705f808d93ddfa01ac3591
botlax <lacsinapaul@gmail.com>
2017-09-05 11:06:57 +0300

    flash update

--- resources/views/vendor/flash/message.blade.php
+++ resources/views/vendor/flash/message.blade.php
@@ -1,15 +1,24 @@
 @if(session('flash_notification'))
 <div class="flash-wrap">
-@foreach ((array) session('flash_notification') as $message)   
-    <div class="flash flash-{{ $message['level'] }} {{ $message['important'] ? 'flash-important' : '' }}">
-        @if ($message['important'])
-            <button type="button"
-                    class="close"
-            ><i class="fa fa-close"></i></button>
-        @endif
+@foreach ((array) session('flash_notification') as $message)
+@php $message = (array)$message[0]; @endphp
+    @if ($message['overlay'])
+        @include('flash::modal', [
+            'modalClass' => 'flash-modal',
+            'title'      => $message['title'],
+            'body'       => $message['message']
+        ])
+    @else
+        <div class="flash flash-{{ $message['level'] }} {{ $message['important'] ? 'flash-important' : '' }}">
+            @if ($message['important'])
+                <button type="button"
+                        class="close"
+                ><i class="fa fa-close"></i></button>
+            @endif
 
-        {!! $message['message'] !!}
-    </div>
+            {!! $message['message'] !!}
+        </div>
+    @endif
 @endforeach
 </div>
 @endif
