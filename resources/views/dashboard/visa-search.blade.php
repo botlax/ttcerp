319a5e2bec97d0fc8f842c1cb933915c29bad242
botlax <lacsinapaul@gmail.com>
2018-01-09 09:49:53 +0300

    Update visa-search.blade.php

--- resources/views/dashboard/visa-search.blade.php
+++ resources/views/dashboard/visa-search.blade.php
@@ -41,6 +41,7 @@ Visas | {{config('app.name')}}
 				{!! Form::label('occupation', 'Occupation') !!}
 				{!! Form::select('occupation',['' => '--Select Occupation--', 
 						'Accountant' => 'Accountant',
+						'Administrator' => 'Administrator',
 						'Arch. Engineer' => 'Arch. Engineer',
 						'Assist. Foreman' => 'Assist. Foreman',
 						'A/C Technician' => 'A/C Technician',
